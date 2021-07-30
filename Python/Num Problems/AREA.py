6763d8599a6ec14dcca9d784bccb34a0fa962c36
Aniket <aniket121397@gmail.com>
2021-07-30 13:44:42 +0530

    area

---
+++ Python/Num Problems/AREA.py
@@ -0,0 +1,22 @@
+r=int(input("Enter radius of cicle:"))
+area=0
+circum=0
+if r>0:
+    area=3.14*r*r
+    print("Area of circle is:",area)
+if r>0:
+    circum=3.14*2*r
+    print("circumfernce of circle is :",circum)
+
+
+
+
+
+
+   
+
+
+
+
+
+
